9e1ce1bb21de529e660f04beb396d5fbad9363b2
Elton Braga Silva <eltonbs19@gmail.com>
2022-12-24 21:52:01 -0300

    config: esqueleto inicial do projeto + controller com hello world

---
+++ agenda-medica/src/main/java/com/elton/spring/clinica/agendamedica/AgendaMedicaApplication.java
@@ -0,0 +1,13 @@
+package com.elton.spring.clinica.agendamedica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaMedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaMedicaApplication.class, args);
+	}
+
+}
---
+++ agenda-medica/src/test/java/com/elton/spring/clinica/agendamedica/AgendaMedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elton.spring.clinica.agendamedica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaMedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
